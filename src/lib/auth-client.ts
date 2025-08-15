2295f08dc8a64bec5f46019bffddd3ff5ebee17f
BH Nibir <biplob.asanibir@gmail.com>
2025-08-15 16:00:19 +0600

    refactor(auth-client): remove baseURL configuration in favor of Next.js rewrite proxy

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from 'better-auth/react';
 
 export const client = createAuthClient({
   /** the base url of the server (optional if you're using the same domain) */
-  baseURL: process.env.NEXT_PUBLIC_BETTER_SERVER,
+  // baseURL: process.env.NEXT_PUBLIC_BETTER_SERVER, no need,  use nextjs rewrite as proxy
   plugins: [adminClient()],
 });
 
