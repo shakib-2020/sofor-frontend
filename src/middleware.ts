7a38bb1981cc4f259e4ea21f3ab6db1260cec487
BH Nibir <biplob.asanibir@gmail.com>
2025-08-16 08:45:26 +0600

    fix(auth): update session cookie handling in middleware
    - Replaced getSessionCookie with getCookieCache for improved session management.
    - Adjusted middleware logic to ensure proper redirection to the sign-in page when no session is found.
    - Updated package dependencies for better-auth to version 1.3.6, including peer dependencies for React and Zod.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,13 @@
-import { getSessionCookie } from 'better-auth/cookies';
+import { getCookieCache } from 'better-auth/cookies';
 import { headers } from 'next/headers';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const session = await getSessionCookie(request.headers);
-
-  if (!session) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
-  }
-  return NextResponse.next();
+  const session = await getCookieCache(request);
+	if (!session) {
+		return NextResponse.redirect(new URL("/sign-in", request.url));
+	}
+	return NextResponse.next();
 }
 
 export const config = {
