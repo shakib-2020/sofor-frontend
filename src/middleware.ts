4cece93c7671b760233e3a38ba5ac7be332e2c94
BH Nibir <biplob.asanibir@gmail.com>
2025-08-16 09:28:14 +0600

    debug(middleware): add session logging to middleware
    - Introduced console logging for session data in the middleware function to aid in debugging session management issues.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,9 @@ import { type NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const session = await getCookieCache(request);
+  
+  console.log("================== Session:", session);
+  
 	if (!session) {
 		return NextResponse.redirect(new URL("/sign-in", request.url));
 	}
