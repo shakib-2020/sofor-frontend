05adba0a943768c479d5191e94c3e70269d4d9bd
Biplob Hasan Nibir <biplob.asanibir@gmail.com>
2025-12-12 09:04:11 +0600

    chore(dependencies): add @better-fetch/fetch package and update version
    - Introduced @better-fetch/fetch dependency at version 1.1.21 to enhance data fetching capabilities.
    - Updated pnpm-lock.yaml to reflect the new dependency and its version.
    - Modified middleware to utilize betterFetch for session handling, improving cookie management and logging.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,26 @@
-import { getSessionCookie } from 'better-auth/cookies';
+import { betterFetch } from '@better-fetch/fetch';
 import { type NextRequest, NextResponse } from 'next/server';
+import type { client } from '@/lib/auth-client';
 
+type Session = typeof client.$Infer.Session;
+export async function middleware(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    '/api/auth/get-session',
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get('cookie') || '', // Forward the cookies from the request
+      },
+    }
+  );
 
-export function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
-  console.log('session in middleware', sessionCookie);
-  if (!sessionCookie) {
+  console.log('session in middleware', session);
+  if (!session) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
   return NextResponse.next();
 }
+
 export const config = {
   matcher: ['/dashboard/:path*', '/profile/:path*', '/my-bookings/:path*'], // Specify the routes the middleware applies to
 };
