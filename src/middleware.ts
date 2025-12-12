146f3483841c9afb20e94e8499fc174d3da922d6
Biplob Hasan Nibir <biplob.asanibir@gmail.com>
2025-12-12 08:52:15 +0600

    refactor(middleware): enhance session logging and specify runtime
    - Added console logging for session details in the middleware for better debugging.
    - Specified 'nodejs' as the runtime in the middleware configuration.
    - Maintained existing route matcher for dashboard, profile, and bookings.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,13 @@ import { type NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const session = await getCookieCache(request);
+  console.log('session in middleware', session);
   if (!session) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
   return NextResponse.next();
 }
 export const config = {
+  runtime: 'nodejs',
   matcher: ['/dashboard/:path*', '/profile/:path*', '/my-bookings/:path*'], // Specify the routes the middleware applies to
 };
