62d32fd9b04184ac7d261003a30b0d440aa990ce
shakib-2020 <nazshakib01@gmail.com>
2026-04-29 01:25:06 +0600

    feat: set baseURL for auth client configuration

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,7 @@ import { adminClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 
 export const client = createAuthClient({
+  baseURL: '/api/auth',
   plugins: [adminClient()],
 });
 
